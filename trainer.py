29f5d02280b1e32828ee523a4f85af20176995c7
kimbyeolhee <qufgml0216@naver.com>
2023-02-24 04:49:02 +0000

    [Feat] add save model

--- trainer.py
+++ trainer.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import pickle
 
 import torch
 import torch.nn.functional as F
@@ -57,56 +56,11 @@ class Trainer:
             batch_loss += loss
 
         return batch_loss / len(epoch_iterator)
-
-
-
-    def valid_per_epoch(self, wiki_dataloader, valid_dataloader, epoch):
-        logger.info("***** Running Validation *****")
-
-        # make context_embeddings
-        c_embs = []
-        with torch.no_grad():
-            epoch_iterator = tqdm(wiki_dataloader, desc="Iteration")
-            self.model.eval()
-
-            for _, batch in enumerate(epoch_iterator):
-                c_inputs = {"input_ids": batch[0].to(self.device), "attention_mask": batch[1].to(self.device), "token_type_ids": batch[2].to(self.device)}
-                outputs = self.model.doc(**c_inputs)
-                c_embs.extend(outputs.cpu().numpy())
-            c_embs = torch.Tensor(c_embs) # (num_wiki, 768)
-
-        if not os.path.exists(f"{path}/saved_models/context_embedding/"):
-            os.makedirs(f"{path}/saved_models/context_embedding/")
-        with open(f"{path}/saved_models/context_embedding/epoch_{epoch+1}.bin", "wb") as f:
-            pickle.dump(c_embs, f)
-            
-        # validation with valid_dataloader
-        top_10 = 0
-
-        with torch.no_grad():
-            epoch_iterator = tqdm(valid_dataloader, desc="Iteration")
-            self.model.eval()
-
-            for _, batch in enumerate(epoch_iterator):
-                q_inputs = {"input_ids": batch[0].to(self.device), "attention_mask": batch[1].to(self.device), "token_type_ids": batch[2].to(self.device)}
-                c_inputs = {"input_ids": batch[3].to(self.device), "attention_mask": batch[4].to(self.device), "token_type_ids": batch[5].to(self.device)}
-                q_embs = self.model.query(**q_inputs)
-
-                # calculate cosine similarity
-                score = torch.matmul(q_embs, c_embs.transpose(0, 1)) # (num_valid, num_wiki)
-                rank = torch.argsort(score, dim=1, descending=True).squeeze() # (num_wiki)
-
-                top_10_passages = [self.wiki_contexts[i] for i in rank[:10]]
-                
-                # top_k accuracy with c_inputs and top_10_passages
-                for i in range(len(c_inputs["input_ids"])):
-                    if c_inputs["input_ids"][i] in top_10_passages:
-                        top_10 += 1
-        
-        return top_10 / len(valid_dataloader)
             
 
     def train(self):
+        best_loss = 1e9
+
         # set model to train mode
         self.model = self.model.to(self.device)
         self.model.train()
@@ -114,10 +68,9 @@ class Trainer:
         # load dataloader
         train_sampler = RandomSampler(self.train_dataset)
         train_dataloader = DataLoader(self.train_dataset, sampler=train_sampler, batch_size=self.config.trainer.per_device_train_batch_size)
-        wiki_dataloader = DataLoader(self.wiki_dataset, batch_size=self.config.trainer.per_device_train_batch_size)
-        valid_dataloader = DataLoader(self.valid_dataset, batch_size=self.config.trainer.per_device_train_batch_size)
+        DataLoader(self.wiki_dataset, batch_size=self.config.trainer.per_device_train_batch_size)
+        DataLoader(self.valid_dataset, batch_size=self.config.trainer.per_device_train_batch_size)
 
-        best_top_10_acc = 0
 
         # load optimizer
         no_decay = ["bias", "LayerNorm.weight"]
@@ -156,14 +109,7 @@ class Trainer:
             train_loss = self.train_per_epoch(epoch_iterator, optimizer, scheduler)
             logger.info("Train loss: {}".format(train_loss))
 
-            # valid per epoch
-            top_10_acc = self.valid_per_epoch(wiki_dataloader, valid_dataloader, epoch)
-            logger.info("Top 10 accuracy: {}".format(top_10_acc))
-
-
-            if top_10_acc > best_top_10_acc:
-                best_top_10_acc = top_10_acc
-                self.model.save_pretrained(f"{path}/saved_models/best_model")
-                logger.info("Best model saved on epoch {}".format(epoch+1))
-
-            
+            # save model if loss is lower than best loss
+            if train_loss < best_loss:
+                best_loss = train_loss
+                self.save_model(f"{path}/saved_model/best_model_{epoch}.pt")
